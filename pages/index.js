66fe20ed059d14a60dfc23a33c3af086657f4504
JorgeLins <jorgelins1155@gmail.com>
2021-01-25 11:05:37 -0300

    codigo ao final da primeira aula react

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 import styled from 'styled-components'
 import db from "../db.json";
-import Widget from "../src/components/widgets"
+import Widget from "../src/components/Widgets"
 import QuizLogo from '../src/components/QuizLogo'
 import QuizBackground from '../src/components/QuizBackground'
 import Footer from '../src/components/Footer'
