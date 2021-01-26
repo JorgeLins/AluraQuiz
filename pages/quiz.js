eda8ef9c9ed9abc82e5e633fb6c228c60b11f502
JorgeLins <jorgelins1155@gmail.com>
2021-01-26 17:12:42 -0300

    Codigo do segundo dia da imersao react

--- pages/quiz.js
+++ pages/quiz.js
@@ -1,8 +1,5 @@
 import React from 'react';
 import styled from 'styled-components';
-import Head from 'next/head';
-import Link from 'next/link';
-import { useRouter } from 'next/router';
 import db from '../db.json';
 import Widget from '../src/components/widgets';
 import QuizLogo from '../src/components/QuizLogo';
@@ -22,52 +19,27 @@ export const QuizContainer = styled.div`
   width: 100%;
   max-width: 350px;
   padding-top: 45px;
-  margin: auto 35%;
+  margin: auto 10%;
   @media screen and (max-width: 500px) {
     margin: auto;
     padding: 15px;
   }
 `;
+
 export default function Home() {
-  const router = useRouter();
-  const [name, setName] = React.useState('');
-  console.log(name, setName);
   return (
-
     <QuizBackground backgroundImage={db.bg}>
-
-      <Head>
-        <title>Alura Quiz - Cinema</title>
-      </Head>
       <QuizContainer>
-
-        <QuizLogo />
         <Widget>
           <Widget.Header>
-            <h1>Movie Quiz Alura</h1>
+            <h1>PErgunta 1</h1>
           </Widget.Header>
 
           <Widget.Content>
-            <form onSubmit={function (infosDoEvento) {
-              infosDoEvento.preventDefault();
-              router.push(`/quiz?name=${name}`);
-
-              console.log('mandando uma submissao por meio do react');
-            }}
-            >
-              <input
-                onChange={function (infosDoEvento) {
-                  console.log(infosDoEvento.target.value);
-                  setName(infosDoEvento.target.value);
-                }}
-                placeholder="Digite o seu nome"
-              />
-              <ButtonIniciar type="submit" disabled={name.length === 0}>
-                Jogar
-                {name}
-              </ButtonIniciar>
-            </form>
+            <p>é o meu quiz sem tema aparente ainda</p>
           </Widget.Content>
+          <ButtonIniciar>Iniciar</ButtonIniciar>
+
         </Widget>
 
         <Widget>
---
+++ src/components/Buttons/index.js
@@ -0,0 +1,21 @@
+import styled from 'styled-components';
+
+const ButtonIniciar = styled.button`
+  /* Adapt the colors based on primary prop */
+  background: ${({ theme }) => theme.colors.primary};
+  color:${({ theme }) => theme.colors.contrastText};
+
+  font-size: 16px;
+  margin-top: 16px;
+  margin-bottom: 16px;
+  margin-right: 40px;
+  margin-left: 40px;
+  padding: 4px 64px;
+  border: 2px solid ${({ theme }) => theme.colors.primary};
+  border-radius: 3px;
+
+
+  
+`;
+
+export default ButtonIniciar;
